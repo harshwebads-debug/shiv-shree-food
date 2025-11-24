d02f082d6eccd1470cdf3d1768e84e30b02b4ddf
harshwebads-debug <harshwebads@example.com>
2025-11-24 17:57:28 +0530

    Initial commit

---
+++ assets/js/animation.js
@@ -0,0 +1,27 @@
+ScrollReveal().reveal('.anim-fade-up', {
+    distance: '40px',
+    duration: 900,
+    easing: 'ease',
+    origin: 'bottom',
+    interval: 120
+});
+
+ScrollReveal().reveal('.anim-slide-left', {
+    distance: '60px',
+    duration: 1000,
+    easing: 'ease',
+    origin: 'left'
+});
+
+ScrollReveal().reveal('.anim-slide-right', {
+    distance: '60px',
+    duration: 1000,
+    easing: 'ease',
+    origin: 'right'
+});
+
+ScrollReveal().reveal('.anim-scale-in', {
+    scale: 0.85,
+    duration: 900,
+    easing: 'ease'
+});
